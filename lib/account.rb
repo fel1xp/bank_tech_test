d8c395b5ab047f98f68fbadf3a06a4ceb2715e7f
Felix Piercy <74382928+fel1xp@users.noreply.github.com>
2021-07-05 12:11:31 +0100

    Improved formatting with rubocop

--- lib/account.rb
+++ lib/account.rb
@@ -1,17 +1,18 @@
+# frozen_string_literal: true
 
-class Account 
+# Account class designed to change user balance
+class Account
   attr_accessor :balance
-  
+
   def initialize
     @balance = 0
   end
 
   def deposit(value)
-    @balance += value 
+    @balance += value
   end
 
   def withdraw(value)
     @balance -= value
   end
-
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,26 +1,27 @@
+# frozen_string_literal: true
+
 require './lib/account'
 
-describe Account do 
-  subject(:account) {described_class.new}
+describe Account do
+  subject(:account) { described_class.new }
 
-  describe '#initialize' do 
-    it 'returns a balance of 0 for a new account' do 
+  describe '#initialize' do
+    it 'returns a balance of 0 for a new account' do
       expect(account.balance).to eq(0)
     end
   end
 
-  describe '#deposit' do 
-    it 'adds money to account' do 
+  describe '#deposit' do
+    it 'adds money to account' do
       account.deposit(100)
       expect(account.balance).to eq(100)
     end
   end
 
   describe '#withdraw' do
-    it 'withdraws money from account' do 
+    it 'withdraws money from account' do
       account.withdraw(100)
       expect(account.balance).to eq(-100)
     end
   end
-
 end
