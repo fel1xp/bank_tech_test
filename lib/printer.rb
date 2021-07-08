0c8f5ea3ba702e3baba2e2d81914ba9cda75b3e3
Felix Piercy <74382928+fel1xp@users.noreply.github.com>
2021-07-08 16:01:57 +0100

    Split Header from Body in Printer

--- lib/printer.rb
+++ lib/printer.rb
@@ -3,8 +3,16 @@ require './lib/account'
 class Printer
 
   def statement 
-    "date || credit || debit || balance
-    14/01/2012 || || 500.00 || 2500.00
+    header
+    body
+  end
+
+  def header 
+    "date || credit || debit || balance"
+  end
+
+  def body
+    "14/01/2012 || || 500.00 || 2500.00
     13/01/2012 || 2000.00 || || 3000.00
     10/01/2012 || 1000.00 || || 1000.00"
   end
